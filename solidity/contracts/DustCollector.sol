80e0ab445bbbb4d8f53fe1451eb5028a894948be
0xRaccoon <raccoon@defi.sucks>
2023-12-05 15:07:55 +0100

    fix: linter
    Signed-off-by: 0xRaccoon <raccoon@defi.sucks>

--- solidity/contracts/DustCollector.sol
+++ solidity/contracts/DustCollector.sol
@@ -15,6 +15,7 @@ abstract contract DustCollector is IDustCollector, Governable {
 
   /// @inheritdoc IDustCollector
   function sendDust(address _token, uint256 _amount, address _to) external onlyGovernor {
+    
     if (_to == address(0)) revert ZeroAddress();
     if (_token == ETH_ADDRESS) payable(_to).transfer(_amount);
     else IERC20(_token).safeTransfer(_to, _amount);
